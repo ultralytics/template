1514e1fbfae718c6d3130346be38852547a31ade
glenn-jocher <glenn.jocher@ultralytics.com>
2023-12-31 20:45:40 +0000

    Auto-format by Ultralytics action

--- template/module1.py
+++ template/module1.py
@@ -2,12 +2,22 @@ def add_numbers(a, b):
     """Add two numbers."""
     return a + b
 
-def some_function_with_a_long_name(argument1, argument2, another_argument, yet_another_argument, one_more_argument, one_last_argument):
+
+def some_function_with_a_long_name(
+    argument1,
+    argument2,
+    another_argument,
+    yet_another_argument,
+    one_more_argument,
+    one_last_argument,
+):
     x = "string1"
-    y = 'string2'
+    y = "string2"
     z = x + y
 
-    
     return z
 
-result = some_function_with_a_long_name(1, 2, 3, 4, 5, 6)
+
+result = some_function_with_a_long_name(
+    1, 2, 3, 4, 5, 6
+)
