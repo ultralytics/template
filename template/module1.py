b5aee880b927e6a641a6efb2ac018295e585d1d7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-12-31 22:34:13 +0100

    Update pyproject.toml

--- template/module1.py
+++ template/module1.py
@@ -10,5 +10,17 @@ def some_function_with_a_long_name(argument1,argument2,another_argument,yet_anot
 
     return z
 
+def get_git_dir():
+    """
+    Determines whether the current file is part of a git repository and if so, returns the repository root directory. If the current file is not part of a git repository, returns None.
+
+    Returns:
+        (Path | None): Git root directory if found or None if not found.
+    """
+    from pathlib import Path
+
+    for d in Path(__file__).parents:
+        if (d / '.git').is_dir():
+            return d
 
 result = some_function_with_a_long_name(1, 2, 3, 4)
