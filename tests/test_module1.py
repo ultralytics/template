1fc42b73d5af903f27d7b4cf2f314b48b08ff02d
glenn-jocher <glenn.jocher@ultralytics.com>
2023-12-31 20:53:34 +0000

    Auto-format by Ultralytics action

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -5,14 +5,6 @@ from module1 import (
 
 def test_add_numbers():
     """Test the add_numbers function from module1."""
-    assert (
-        add_numbers(2, 3) == 5
-    )
-    assert (
-        add_numbers(-1, 1)
-        == 0
-    )
-    assert (
-        add_numbers(-1, -1)
-        == -2
-    )
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(-1, -1) == -2
