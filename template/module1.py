85a7e70bfa9165669b514dec36a9c9454dc7a568
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-22 14:22:47 +0100

    Update functions

--- template/module1.py
+++ template/module1.py
@@ -2,29 +2,14 @@
 
 
 def add_numbers(a, b):
-    """Add two numbers."""
-
-    return a + b
-
-
-def some_function_with_a_long_name(argument1, argument2, another_argument, yet_another_argument):
-    x = "string1"
-    y = "string2"
-    z = x + y
-
-    return z
-
-
-def get_git_dir():
     """
-    Determines whether the current file is part of a git repository and if so, returns the repository root directory. If
-    the current file is not part of a git repository, returns None.
+    Adds two numbers and returns the sum.
+
+    Args:
+        a (float | int): The first number to add.
+        b (float | int): The second number to add.
 
     Returns:
-        (Path | None): Git root directory if found or None if not found.
+        (float | int): The sum of `a` and `b`.
     """
-    from pathlib import Path
-
-    for d in Path(__file__).parents:
-        if (d / ".git").is_dir():
-            return d
+    return a + b
