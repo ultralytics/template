b8b62fe6db0007c76e6180dc9e5c17d49dec289d
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-05-10 13:36:19 +0200

    Add code coverage (#58)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

---
+++ tests/test_with_unittest.py
@@ -0,0 +1,16 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
+import unittest
+from template.module1 import add_numbers
+
+class TestModule1(unittest.TestCase):
+    """Test cases for module1 functions."""
+    
+    def test_add_numbers(self):
+        """Tests add_numbers function with positive and negative integers."""
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(-1, -1), -2)
+
+if __name__ == "__main__":
+    unittest.main()
